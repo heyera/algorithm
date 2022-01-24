c73e2e38039d58b4e488002dbb53295578cd3cd9
heyera <63634801+heyera@users.noreply.github.com>
2022-01-24 23:20:00 +0900

    Add files via upload

--- baekjoon/bj11650.java
+++ baekjoon/bj11650.java
@@ -1,43 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class bj11650 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        int n= Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        for(int i=0;i<n;i++){
-            int x=Integer.parseInt(br.readLine());
-
-            
-            
-
-        }
-        
-
-        
-    }
-    static class point implements Comparable<point>{
-        int x,y;
-        public point(int x, int y){
-            this.x=x;
-            this.y=y;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] xy = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            String point = br.readLine();
+            StringTokenizer st = new StringTokenizer(point, " ");
+            xy[i][0]=Integer.parseInt(st.nextToken());
+            xy[i][1]=Integer.parseInt(st.nextToken());
         }
 
-        @Override
-        public int compareTo(point p){
-            if(this.x>p.x){
-                return x;
-            }
-            else if(this.x==p.x){
-                if(this.y>p.y){
-                    return 1;
-                }
-            }
-            return -1;
+        Arrays.sort(xy,(x,y)->{
+            if(x[0]==y[0])
+                return x[1]-y[1];
+            else
+                return x[0]-y[0];
+        });
+        for (int i = 0; i < n; i++) {
+			System.out.println(xy[i][0] + " " + xy[i][1]);
+		}
 
-        }
     }
 }
