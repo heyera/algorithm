0338e1dc4c161dfee41d650c34b7a58c9541c38c
heyera <63634801+heyera@users.noreply.github.com>
2023-02-17 20:48:14 +0900

    Add files via upload

---
+++ baekjoon/bj10826.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class bj10826 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        BigInteger arr[] = new BigInteger[n + 2];
+        arr[0] = BigInteger.ZERO;
+        arr[1] = BigInteger.ONE;
+
+        for (int i = 2; i < n + 1; i++) {
+            arr[i] = arr[i - 1].add(arr[i - 2]);
+        }
+        System.out.println(arr[n]);
+    }
+}
