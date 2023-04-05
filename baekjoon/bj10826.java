6f567186bb899d0a178f68ff4ea47c1d7baa57c5
heyera <63634801+heyera@users.noreply.github.com>
2023-04-05 17:33:50 +0900

    DP_JAVA

--- baekjoon/bj10826.java
+++ baekjoon/bj10826.java
@@ -14,6 +14,7 @@ public class bj10826 {
         for (int i = 2; i < n + 1; i++) {
             arr[i] = arr[i - 1].add(arr[i - 2]);
         }
+        
         System.out.println(arr[n]);
     }
 }
