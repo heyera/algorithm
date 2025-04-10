79737c1e4435bc87f08664ecd6edd3bd5ef0496b
heyera <63634801+heyera@users.noreply.github.com>
2025-04-10 22:37:26 +0900

    Modify

--- baekjoon/bj3151.kt
+++ baekjoon/bj3151.kt
@@ -4,28 +4,48 @@ import java.io.InputStreamReader
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
-    val scoreList = br.readLine().split(" ").map { it.toInt() }.toMutableList()
+    val scoreList = br.readLine().split(" ").map { it.toInt() }.sorted()
 
-    var count = 0
-    scoreList.sort()
+    var count = 0L
 
-    for (i in 0 until n) {
+    for (i in 0 until n - 2) {
         var left = i + 1
         var right = n - 1
 
         while (left < right) {
             val sum = scoreList[i] + scoreList[left] + scoreList[right]
 
-            if (sum == 0) {
-                count += 1
-                left += 1
-            } else if (sum < 0)
-                left += 1
-            else
-                right -= 1
+            when {
+                sum == 0 -> {
+                    if (scoreList[left] == scoreList[right]) {
+                        val cnt = right - left + 1
+                        count += cnt.toLong() * (cnt - 1) / 2
+                        break
+                    }
+
+                    var leftCount = 1
+                    var rightCount = 1
+
+                    while (left + 1 < right && scoreList[left] == scoreList[left + 1]) {
+                        leftCount++
+                        left++
+                    }
+
+                    while (right - 1 > left && scoreList[right] == scoreList[right - 1]) {
+                        rightCount++
+                        right--
+                    }
+
+                    count += leftCount.toLong() * rightCount
+                    left++
+                    right--
+                }
+
+                sum < 0 -> left++
+                else -> right--
+            }
         }
     }
-
 
     println(count)
 }
