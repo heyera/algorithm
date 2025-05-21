63679d04c2c2cd4a730e2947cfcbf49ca40a0ef2
heyera <63634801+heyera@users.noreply.github.com>
2025-05-21 22:57:37 +0900

    DFS_Kotlin

---
+++ baekjoon/bj1038.kt
@@ -0,0 +1,29 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+val result = mutableListOf<Long>()
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+
+    for (i in 0..9) {
+        dfs(i.toLong(), i)
+    }
+
+    result.sort()
+
+    if (n >= result.size) {
+        println(-1)
+    } else {
+        println(result[n])
+    }
+}
+
+private fun dfs(num: Long, last: Int) {
+    result.add(num)
+
+    for (i in 0 until last) {
+        dfs(num * 10 + i, i)
+    }
+}
